1f08d8075f60b994ba9da92cba9cc8b59740253d
KKT <kimgt2828@gmail.com>
2025-07-23 19:54:17 +0900

    Feat: 멤버 엔티티 추가

---
+++ src/main/java/com/example/websocket_study/entity/Member.java
@@ -0,0 +1,16 @@
+package com.example.websocket_study.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+}
